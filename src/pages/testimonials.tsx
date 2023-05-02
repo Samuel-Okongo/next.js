7359dc011c6fd2eee05821a9edf95322433889b2
samuelokongo <so274@njit.ord>
2023-05-02 16:33:15 -0400

    added testimonials

---
+++ src/pages/testimonials.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+const Testimonials = () => {
+  return (
+    <div>
+      <h1>Testimonials</h1>
+      <p>Here are some testimonials from our users:</p>
+      <ul>
+        <li>"This website has helped me integrate AI into my teaching methods!" - Sarah</li>
+        <li>"I've learned so much about AI and how it can enhance my students' learning experience." - Miguel</li>
+        <li>"The resources on this website have made it easy for me to start using AI in my classroom, even without coding experience." - Laura</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Testimonials;
