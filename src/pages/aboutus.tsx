4b09e5239cccfa23f1c923eddc6e1b552351a876
Devendra Pitam <dp787@njit.edu>
2023-05-03 15:43:13 -0400

    added our mission

--- src/pages/aboutus.tsx
+++ src/pages/aboutus.tsx
@@ -13,13 +13,17 @@ const About: React.FC = () => {
         <div className="max-w-4xl px-4 py-8 bg-white shadow-md rounded-md">
           <h1 className="text-4xl font-bold text-gray-800 mb-4">About Us</h1>
           <p className="text-gray-600 text-lg mb-4">
-            We are a team of educators, technologists, and AI enthusiasts who are passionate about empowering teachers with the knowledge and tools necessary to integrate Artificial Intelligence into their classrooms. With years of experience in education and technology, we understand the unique challenges and opportunities that AI presents for educators and learners alike.
+            We are a team of passionate professionals dedicated to providing the best products and services to our customers. With years of experience in our respective fields, we strive to meet and exceed the expectations of our clients.
           </p>
           <p className="text-gray-600 text-lg mb-4">
-            Our mission is to bridge the gap between AI and education by providing high-quality resources, practical guidance, and a supportive community for educators interested in embracing AI. We believe in the power of AI to transform teaching and learning, and we are dedicated to helping educators harness this potential to create engaging and meaningful experiences for their students.
+            Our mission is to create an exceptional experience for our customers by offering high-quality products, unparalleled customer service, and innovative solutions. We believe in building lasting relationships with our customers and partners, and we continually invest in the growth and development of our team members.
+          </p>
+          <h2 className="text-2xl font-bold text-gray-800 mb-4">Our Mission:</h2>
+          <p className="text-gray-600 text-lg mb-4">
+            We strive to make learning a better experience for not only students but for teachers as well. So, all participants are in a safe and enjoyable learning environment. We want what is best for teachers to educate students for their future. And prep those said students to be the best generation and forth.
           </p>
           <p className="text-gray-600 text-lg">
-            Thank you for joining us on this journey, and we look forward to supporting you as you explore the exciting world of AI in education!
+            Thank you for considering us as your partner, and we look forward to working with you!
           </p>
         </div>
       </div>
