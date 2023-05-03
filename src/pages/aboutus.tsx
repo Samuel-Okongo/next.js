134dfa76e867685876690a9c05c2a371cb3d5047
samuelokongo <so274@njit.ord>
2023-05-03 14:54:40 -0400

    added sign up page

--- src/pages/aboutus.tsx
+++ src/pages/aboutus.tsx
@@ -1,19 +1,30 @@
+// pages/about.tsx
+
 import React from 'react';
 import Head from 'next/head';
 
-
-const AboutUs = () => {
+const About: React.FC = () => {
   return (
-    <div>
-      <h1>About Us</h1>
-      <p>Here are some testimonials from our users:</p>
-      <ul>
-        <li>"This website has helped me integrate AI into my teaching methods!" - Sarah</li>
-        <li>"I've learned so much about AI and how it can enhance my students' learning experience." - Miguel</li>
-        <li>"The resources on this website have made it easy for me to start using AI in my classroom, even without coding experience." - Laura</li>
-      </ul>
-    </div>
+    <>
+      <Head>
+        <title>About Us</title>
+      </Head>
+      <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
+        <div className="max-w-4xl px-4 py-8 bg-white shadow-md rounded-md">
+          <h1 className="text-4xl font-bold text-gray-800 mb-4">About Us</h1>
+          <p className="text-gray-600 text-lg mb-4">
+            We are a team of educators, technologists, and AI enthusiasts who are passionate about empowering teachers with the knowledge and tools necessary to integrate Artificial Intelligence into their classrooms. With years of experience in education and technology, we understand the unique challenges and opportunities that AI presents for educators and learners alike.
+          </p>
+          <p className="text-gray-600 text-lg mb-4">
+            Our mission is to bridge the gap between AI and education by providing high-quality resources, practical guidance, and a supportive community for educators interested in embracing AI. We believe in the power of AI to transform teaching and learning, and we are dedicated to helping educators harness this potential to create engaging and meaningful experiences for their students.
+          </p>
+          <p className="text-gray-600 text-lg">
+            Thank you for joining us on this journey, and we look forward to supporting you as you explore the exciting world of AI in education!
+          </p>
+        </div>
+      </div>
+    </>
   );
 };
 
-export default AboutUs;
+export default About;
