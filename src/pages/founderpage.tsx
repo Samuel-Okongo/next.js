caa226e77db555e2ec023c921e76ddd8ec2ea29b
samuelokongo <so274@njit.ord>
2023-05-10 14:48:58 -0400

    fixed founders page

---
+++ src/pages/founderpage.tsx
@@ -0,0 +1,98 @@
+import React from 'react';
+
+const pageContainerStyle = {
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  backgroundColor: '#fafafa',
+  fontFamily: 'Arial, sans-serif',
+  color: '#333',
+  minHeight: '100vh',
+};
+
+const headerStyle = {
+  backgroundColor: '#0070f3',
+  width: '100%',
+  padding: '20px 0',
+  color: 'white',
+  fontSize: '1.5em',
+  textAlign: 'center',
+  fontWeight: 'bold',
+  position: 'sticky',
+  top: '0',
+  zIndex: '10',
+};
+
+const founderCardStyle = {
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  justifyContent: 'center',
+  backgroundColor: '#fff',
+  border: '1px solid #ddd',
+  borderRadius: '5px',
+  margin: '20px',
+  padding: '20px',
+  boxShadow: '0 2px 5px rgba(0, 0, 0, 0.15)',
+  maxWidth: '800px',
+  width: '90%',
+};
+
+const founderNameStyle = {
+  fontSize: '1.5em',
+  fontWeight: 'bold',
+  color: '#0070f3',
+};
+
+const founderDescriptionStyle = {
+  marginTop: '10px',
+  fontSize: '1.2em',
+  textAlign: 'center',
+};
+
+const footerStyle = {
+  backgroundColor: '#0070f3',
+  width: '100%',
+  padding: '20px 0',
+  color: 'white',
+  fontSize: '1em',
+  textAlign: 'center',
+  fontWeight: 'bold',
+  position: 'sticky',
+  bottom: '0',
+};
+
+const FounderPage = () => {
+  const founders = [
+    {
+      name: 'Samuel Okongo',
+      description:
+        'Samuel is a software engineer with a passion for AI and education. He believes that every teacher should have access to the latest AI tools to make their job easier and more effective.',
+    },
+    {
+      name: 'Devendra Pitam',
+      description:
+        'Devendra is a web developer who loves to code. He was excited to work on this project to help teachers understand and use AI in their classrooms.',
+    },
+    {
+      name: 'Christopher Rameirez',
+      description:
+        'Christopher is a full-stack developer who has always been fascinated by AI. He believes that AI has the potential to revolutionize teaching and learning.',
+    },
+  ];
+
+  return (
+    <div style={pageContainerStyle}>
+      <header style={headerStyle}>Our Founders</header>
+      {founders.map((founder, index) => (
+        <div key={index} style={founderCardStyle}>
+          <div style={founderNameStyle}>{founder.name}</div>
+          <div style={founderDescriptionStyle}>{founder.description}</div>
+        </div>
+      ))}
+      <footer style={footerStyle}>© 2023 AI For Teachers. All rights reserved.</footer>
+    </div>
+  );
+};
+
+export default FounderPage;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import Link from 'next/link'
 import Image from 'next/image'
-import searchbar from '../components/searchbar';
+import SearchBar from '../components/searchbar.js';
 import { Inter } from 'next/font/google'
 import styles from '../styles/SearchBar.module.css'
 
@@ -9,20 +9,20 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <searchbar onSearch={(searchTerm) => console.log(searchTerm)} />
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+        <searchbar className="searchbar-top-right" onSearch={(searchTerm) => console.log(searchTerm)} />
+        <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-black-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Get started by clicking this&nbsp;
           <a href="http://localhost:3000/signup" class="font-mono font-bold">Signup</a>
         </p>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
             className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
+            href="http://localhost:3000/founderpage"
             target="_blank"
             rel="noopener noreferrer"
           >
-            By{'Samuel,Chris,Dev'}
+            {'Founders'}
             <Image
               src="/vercel.svg"
               alt="Vercel Logo"
@@ -34,7 +34,7 @@ export default function Home() {
           </a>
         </div>
       </div>
-<h1 className="title">
+      <h1 className="title">
   Read <Link href="/posts/first-post">this page!</Link>
 </h1>
 
