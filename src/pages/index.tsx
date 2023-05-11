2f58aeb9a31d38b4e79baa0e235cd2d6a7cafd7d
cramsterr <cr432@njit.edu>
2023-05-10 22:46:48 -0400

    changed logo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -146,7 +146,7 @@ export default function Home() {
         </a>
 
         <a
-          href="src/pages/community.html"
+          href="http://localhost:63342/next.js/src/pages/community.html?_ijt=mrmne0o3pnhskt4u8a284c9pfv&_ij_reload=RELOAD_ON_SAVE"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
           rel="noopener noreferrer"
